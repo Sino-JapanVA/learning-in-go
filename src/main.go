b194305ee8ecb35698c93d278308bc5eb0c1d0b6
Treasure <treasurewmj@foxmail.com>
2018-04-30 23:42:49 +0800

    完善类型系统

--- src/main.go
+++ src/main.go
@@ -4,13 +4,15 @@ import (
 	"fmt"
 )
 
+type user struct {
+	name string
+	age  int
+}
+
 func main() {
-	array := [5]int{1, 2, 3, 4, 5}
-	array2 := [...]int{1, 2}
-	slice := array[0:3:3]
-	fmt.Println(slice)
-	slice = append(slice, 11)
-	slice[0] = 0
-	fmt.Println(slice, cap(slice))
-	fmt.Println(array)
+	age := 22
+	lisa := user{
+		name: "Lisa",
+		age:  age}
+	fmt.Println(lisa)
 }
