ae15fc5eba31cf5521a988c94e1f668721faea7e
Treasure <treasurewmj@foxmail.com>
2018-05-02 14:11:18 +0800

    补充JSON以及日志工具的使用

--- src/main.go
+++ src/main.go
@@ -4,15 +4,34 @@ import (
 	"fmt"
 )
 
-type user struct {
-	name string
-	age  int
-}
+type (
+	gResult struct {
+		GsearchResultClass string `json:"GsearchResultClass"`
+		UnescapedURL       string `json:"unescapedUrl"`
+		URL                string `json:"url"`
+		VisibleURL         string `json:"visibleUrl"`
+		CacheURL           string `json:"cacheUrl"`
+		Title              string `json:"title"`
+		TitleNoFormatting  string `json:"titleNoFormatting"`
+		Content            string `json:"content"`
+	}
+
+	gResponse struct {
+		ResponseData struct {
+			Results []gResult `json:"results"`
+		} `json:"responseData"`
+	}
+)
 
 func main() {
-	age := 22
-	lisa := user{
-		name: "Lisa",
-		age:  age}
-	fmt.Println(lisa)
+	x := [...]int{1, 2, 3, 4}
+	// y := []int{5, 6, 7, 8}
+	array(x)
+	// array(y)
+}
+
+func array(p [4]int) {
+	for index, val := range p {
+		fmt.Printf("index:%d  val:%d\n", index, val)
+	}
 }
