cefd8a40fde185adc51a2302e054ef28bba72678
Treasure <treasurewmj@foxmail.com>
2018-05-06 15:52:42 +0800

    更新字符串操作

--- src/basic/main/main.go
+++ src/basic/main/main.go
@@ -1,19 +1,28 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
-	"io/ioutil"
+	"strings"
 )
 
 func main() {
-	lrc, err := ioutil.ReadFile("../告白气球.txt")
-	// txt := string(lrc)
-	runeData := bytes.Runes(lrc)
-	// runeTxt := string(runeData)
-	checkErr(err)
-	fmt.Println(len(lrc))
-	fmt.Println(len(runeData))
+	// lrc, err := ioutil.ReadFile("../告白气球.txt")
+	// // txt := string(lrc)
+	// runeData := bytes.Runes(lrc)
+	// // runeTxt := string(runeData)
+	// checkErr(err)
+	// fmt.Println(len(lrc))
+	// fmt.Println(len(runeData))
+	lrc := "ss超凡蜘蛛侠"
+	lrcRune := []rune(lrc)
+	txt := "蜘"
+	txtRune := []rune(txt)
+	n := strings.IndexRune(lrc, txtRune[0])
+	fmt.Println(n)
+	for index, val := range lrcRune {
+		fmt.Printf("%v  %d\n", string(val), index)
+	}
+	fmt.Println(string(lrcRune[4]))
 }
 
 func checkErr(err error) {
