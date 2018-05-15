6c1f2278509b3561bb3dc5367d9c9c7a10ba8500
Treasure <treasurewmj@foxmail.com>
2018-05-15 14:41:34 +0800

    初始化beego

---
+++ src/bee-go/routers/router.go
@@ -0,0 +1,14 @@
+package routers
+
+import (
+	"bee-go/controllers"
+
+	"github.com/astaxie/beego"
+)
+
+func init() {
+	beego.Router("/", &controllers.MainController{})
+	beego.Post("/add", func()  {
+		
+	})
+}
